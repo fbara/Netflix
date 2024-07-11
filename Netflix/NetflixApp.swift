3d7da9d738912f9ff99b245b0a6c8801cf00ac5f
fbara <fbara1@yahoo.com>
2024-07-11 11:05:34 -0400

    Initial Commit

---
+++ Netflix/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Netflix
+//
+//  Created by Frank Bara on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
