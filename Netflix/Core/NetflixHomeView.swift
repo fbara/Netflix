4cd7d7e4000ba2c22db932e656577fcbb8fd60e7
fbara <fbara1@yahoo.com>
2024-07-11 11:30:47 -0400

    First commit.  Added colors.

---
+++ Netflix/Core/NetflixHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  NetflixHomeView.swift
+//  Netflix
+//
+//  Created by Frank Bara on 7/11/24.
+//
+
+import SwiftUI
+
+struct NetflixHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetflixHomeView()
+}
